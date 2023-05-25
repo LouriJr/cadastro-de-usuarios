378b214c22aa0eca97da0423ec79203f99db4787
Louri <Louri@niuco.com.br>
2023-05-25 18:12:36 -0300

    Adicionando authenticação na API

---
+++ backend/CadastroDeUsuarios-API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CadastroDeUsuariosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Login")]
+        public IActionResult Cadastrar()
+        {
+
+            var token = GenerateJwtToken("", "PU8a9W4sv2opkqlOwmgsn3w3Innlc4D5");
+            return Ok(token);
+        }
+
+        string GenerateJwtToken(string username, string secretKey)
+        {
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "nome-do-usuario"),
+                    new Claim(ClaimTypes.Email, "email-do-usuario"),
+
+                };
+
+
+            var token = new JwtSecurityToken(
+                "","",
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- backend/CadastroDeUsuarios-API/Startup.cs
+++ backend/CadastroDeUsuarios-API/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -6,9 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CadastroDeUsuarios_API
@@ -25,6 +28,26 @@ namespace CadastroDeUsuarios_API
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+
+			var secretKey = "PU8a9W4sv2opkqlOwmgsn3w3Innlc4D5";
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = key,
+					ValidateIssuer = false,
+					ValidateAudience = false
+				};
+			});
+
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
@@ -44,6 +67,7 @@ namespace CadastroDeUsuarios_API
 			.AllowAnyOrigin()
 			.AllowAnyMethod()
 			.AllowAnyHeader());
+
 
 			app.UseHttpsRedirection();
 			app.UseMvc();
